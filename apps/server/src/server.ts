3be34d11389a883bfbc41833ba43b3b3776575d1
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-03-31 12:51:27 +1300

    feat(server): add trpc generated docs at /trpc-panel

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,6 +1,7 @@
 import { createExpressMiddleware as createTrpcExpressMiddleware } from "@trpc/server/adapters/express";
 import cors from "cors";
 import express from "express";
+import { renderTrpcPanel } from "trpc-panel";
 
 import { appRouter, createTRPCContext } from "@acme/api";
 
@@ -28,6 +29,15 @@ export async function makeExpressServer() {
       createContext: createTRPCContext,
     }),
   );
+
+  app.use("/trpc-panel", (_, res) => {
+    res.send(
+      renderTrpcPanel(appRouter, {
+        url: "http://localhost:4000/trpc",
+        transformer: "superjson",
+      }),
+    );
+  });
 
   // health check
   app.get("/health", (_, res) => {
