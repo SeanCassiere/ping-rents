4906f6dc8f079e8c65901a100ceb4d6904d48432
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-03 20:56:06 +1200

    feat(api): add bindings auth-service login functionality

--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -2,16 +2,31 @@ import { TRPCError } from "@trpc/server";
 
 import { AuthService } from "@acme/auth";
 import { z } from "@acme/validator";
-import { RegisterNewCompanyAndAccountSchema } from "@acme/validator/src/auth";
+import {
+  COOKIE_SESSION_ID_IDENTIFIER,
+  LoginWithCompanyAndUserSchema,
+  RegisterNewCompanyAndAccountSchema,
+} from "@acme/validator/src/auth";
 
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const authRouter = createTRPCRouter({
+  registerCompanyAndAccount: publicProcedure
+    .input(RegisterNewCompanyAndAccountSchema)
+    .mutation(async ({ input }) => {
+      try {
+        return await AuthService.registerNewCompanyAndAccount(input);
+      } catch (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: (error as any)?.message ?? "Something went wrong", // eslint-disable-line
+        });
+      }
+    }),
   getSession: publicProcedure.query(({ ctx }) => {
     return ctx.user;
   }),
   getSecretMessage: protectedProcedure.query(() => {
-    // testing type validation of overridden next-auth Session in @acme/auth package
     return "you can see this secret message!";
   }),
   getProtectedUser: protectedProcedure.query(({ ctx }) => {
@@ -47,11 +62,16 @@ export const authRouter = createTRPCRouter({
         });
       }
     }),
-  registerCompanyAndAccount: publicProcedure
-    .input(RegisterNewCompanyAndAccountSchema)
-    .mutation(async ({ input }) => {
+  loginWithAccessCode: publicProcedure
+    .input(LoginWithCompanyAndUserSchema)
+    .mutation(async ({ input, ctx }) => {
       try {
-        return await AuthService.registerNewCompanyAndAccount(input);
+        const result = await AuthService.userLoginWithAccessCode(input);
+        ctx.res.cookie(COOKIE_SESSION_ID_IDENTIFIER, result.sessionId, {
+          httpOnly: true,
+        });
+
+        return result;
       } catch (error) {
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
@@ -59,4 +79,8 @@ export const authRouter = createTRPCRouter({
         });
       }
     }),
+  logout: publicProcedure.mutation(({ ctx }) => {
+    ctx.res.clearCookie(COOKIE_SESSION_ID_IDENTIFIER);
+    return { success: true };
+  }),
 });
