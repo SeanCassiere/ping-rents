7fb44fff5a69fc36654dc15d721f27be704e7e7d
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-03 16:59:07 +1200

    feat(auth): set minimum length for name field when registering

--- packages/validator/src/auth.ts
+++ packages/validator/src/auth.ts
@@ -1,8 +1,8 @@
 import { z } from "./zod-project";
 
 export const RegisterNewCompanyAndAccountSchema = z.object({
-  companyName: z.string(),
-  accountName: z.string(),
+  companyName: z.string().min(1),
+  accountName: z.string().min(1),
   accountEmail: z.string().email(),
 });
 export type InputRegisterNewCompanyAndAccount = z.infer<
