13b2958f77439e1f414794a54f48f5855f6c1a24
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-04 12:53:03 +1200

    feat(api): implement functionality for vehicle types (#15)
    * feat(db): add vehicle-type to the db schema
    
    * feat(api): implement vehicle-types functionality

---
+++ packages/validator/src/vehicleType.ts
@@ -0,0 +1,12 @@
+import { z } from "./zod-project";
+
+export const CreateVehicleTypeSchema = z.object({
+  name: z.string().min(1),
+});
+export type InputCreateVehicleType = z.infer<typeof CreateVehicleTypeSchema>;
+
+export const UpdateVehicleTypeSchema = z.object({
+  vehicleTypeId: z.string().min(1),
+  name: z.string().min(1),
+});
+export type InputUpdateVehicleType = z.infer<typeof UpdateVehicleTypeSchema>;
