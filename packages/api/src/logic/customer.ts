43a5ea3ac940458b7d545bf3a519947140aaf99e
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-04 13:48:43 +1200

    feat(api): implement functionality for customer

---
+++ packages/api/src/logic/customer.ts
@@ -0,0 +1,70 @@
+import { prisma } from "@acme/db";
+import type {
+  InputCreateCustomer,
+  InputUpdateCustomer,
+} from "@acme/validator/src/customer";
+
+import type { AuthMetaUser } from "../trpc";
+
+class CustomerController {
+  public async getAll(user: AuthMetaUser) {
+    return await prisma.customer
+      .findMany({
+        where: { companyId: user.companyId },
+        orderBy: { firstName: "asc" },
+      })
+      .then((results) =>
+        results.map((customer) => ({
+          ...customer,
+          fullName: `${customer.firstName} ${customer.lastName}`.trim(),
+        })),
+      );
+  }
+
+  public async create(user: AuthMetaUser, payload: InputCreateCustomer) {
+    const customer = await prisma.customer.upsert({
+      where: {
+        email_companyId: {
+          email: payload.email.toLowerCase(),
+          companyId: user.companyId,
+        },
+      },
+      update: {
+        firstName: payload.firstName,
+        lastName: payload.lastName,
+        email: payload.email.toLowerCase(),
+      },
+      create: {
+        firstName: payload.firstName,
+        lastName: payload.lastName,
+        email: payload.email.toLowerCase(),
+        company: { connect: { id: user.companyId } },
+      },
+    });
+
+    return {
+      ...customer,
+      fullName: `${customer.firstName} ${customer.lastName}`.trim(),
+    };
+  }
+
+  public async updateById(_: AuthMetaUser, payload: InputUpdateCustomer) {
+    const customer = await prisma.customer.update({
+      where: {
+        id: payload.id,
+      },
+      data: {
+        firstName: payload.firstName,
+        lastName: payload.lastName,
+        email: payload.email.toLowerCase(),
+      },
+    });
+
+    return {
+      ...customer,
+      fullName: `${customer.firstName} ${customer.lastName}`.trim(),
+    };
+  }
+}
+
+export const CustomerLogic = new CustomerController();
---
+++ packages/api/src/router/customer.ts
@@ -0,0 +1,24 @@
+import "@acme/validator/src/vehicleType";
+import {
+  CreateCustomerSchema,
+  UpdateCustomerSchema,
+} from "@acme/validator/src/customer";
+
+import { CustomerLogic } from "../logic/customer";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const customersRouter = createTRPCRouter({
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return CustomerLogic.getAll(ctx.user);
+  }),
+  create: protectedProcedure
+    .input(CreateCustomerSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await CustomerLogic.create(ctx.user, input);
+    }),
+  updateById: protectedProcedure
+    .input(UpdateCustomerSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await CustomerLogic.updateById(ctx.user, input);
+    }),
+});
