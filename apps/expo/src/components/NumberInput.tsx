2b14c2ddeb77c483ae7b989d19ff66ce3372b442
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-08 18:27:03 +1200

    fix(app): isomorphic rendering of the number input

--- apps/expo/src/components/NumberInput.tsx
+++ apps/expo/src/components/NumberInput.tsx
@@ -1,36 +1,26 @@
 import React from "react";
-import { Text, View } from "react-native";
-import NumericInput, {
-  type INumericInputProps,
-} from "react-native-numeric-input";
-import { Controller, type Control } from "react-hook-form";
+import { Platform, Text, View } from "react-native";
+import NumericInput from "react-native-numeric-input";
+import {
+  Controller,
+  type Control,
+  type UseFormRegister,
+} from "react-hook-form";
 
-const NumberInput = (
-  props: {
-    control: Control<any>;
-    name: string;
-    label: string;
-    decimal?: boolean;
-    disabled?: boolean;
-  } & Omit<
-    INumericInputProps,
-    | "value"
-    | "onChange"
-    | "valueType"
-    | "type"
-    | "containerStyle"
-    | "inputStyle"
-    | "editable"
-  >,
-) => {
-  const {
-    control,
-    name,
-    label,
-    decimal,
-    disabled = false,
-    ...inputProps
-  } = props;
+type NumberInputProps = {
+  control: Control<any>;
+  register: UseFormRegister<any>;
+  name: string;
+  label: string;
+  decimal?: boolean;
+  disabled?: boolean;
+  minValue?: number;
+  maxValue?: number;
+  placeHolder?: string;
+};
+
+const MobileNumberInput = (props: NumberInputProps) => {
+  const { control, name, label, decimal, disabled = false } = props;
   return (
     <Controller
       control={control}
@@ -56,7 +46,6 @@ const NumberInput = (
               fontSize: 12,
             }}
             editable={!disabled}
-            {...inputProps}
           />
           {error && <Text style={{ color: "red" }}>{error.message}</Text>}
         </View>
@@ -64,5 +53,58 @@ const NumberInput = (
     />
   );
 };
+
+const WebNumberInput = (props: NumberInputProps) => {
+  const {
+    control,
+    name,
+    label,
+    register,
+    disabled = false,
+    placeHolder,
+  } = props;
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ fieldState: { error } }) => (
+        <View style={{ width: "100%" }}>
+          <Text style={{ paddingBottom: 6, fontSize: 15, fontWeight: "400" }}>
+            {label}
+          </Text>
+          <input
+            type="number"
+            {...register(name, { valueAsNumber: true })}
+            disabled={disabled}
+            style={{
+              fontSize: 12,
+              color: "rgb(23, 23, 23)",
+              lineHeight: "1.5em",
+              fontWeight: 400,
+              paddingTop: "8px",
+              paddingBottom: "9px",
+              paddingLeft: "12px",
+              paddingRight: "12px",
+              width: "100%",
+              appearance: "none",
+              resize: "none",
+              boxSizing: "border-box",
+              borderColor: "rgb(212, 212, 212)",
+              borderRadius: "4px",
+              borderWidth: "1px",
+              textDecoration: "none",
+              fontStyle: "normal",
+            }}
+            step={0.01}
+            placeholder={placeHolder}
+          />
+          {error && <Text style={{ color: "red" }}>{error.message}</Text>}
+        </View>
+      )}
+    />
+  );
+};
+
+const NumberInput = Platform.OS === "web" ? WebNumberInput : MobileNumberInput;
 
 export default NumberInput;
