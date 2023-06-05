3d1c7635d2f694f7a4cd8c2d49ba7ff0ca05f606
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-06-05 12:30:07 +1200

    feat(app): implement a common datetime picker for the user input (#73)
    * feat(app): add react-native-modal-datetime-picker component package
    
    * feat(app): implement date-time input using button as a base

--- apps/expo/src/utils/dates.ts
+++ apps/expo/src/utils/dates.ts
@@ -4,4 +4,7 @@ export const DateFormatter = {
   rentalListView: (date: Date) => {
     return format(date, "dd/MM/yyyy HH:mm a");
   },
+  dateTimePickerView: (date: Date) => {
+    return format(date, "dd/MM/yyyy HH:mm a");
+  },
 };
